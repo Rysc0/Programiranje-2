4b865f815b2e2992d3e3f91b5e379ef3120e3014
Rysc0 <boris.bastek@gmail.com>
2020-10-20 22:44:20 +0200

    Init commit

---
+++ Lab03/BinaryTree.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "biblioteka_vrijeme.cc"        
+using namespace std;
+ 
+
+
+
+
+int main(){
+
+
+
+
+int jos;
+do{
+    cout <<"----------------------------------------------------------------------" << endl;
+    cout << "1. Dodavanje elementa na kraj dvostruko vezane liste, ispis liste od početka" << endl;
+    cout << "2. Dodavanje elementa na početak dvostruko vezane liste, ispis liste od kraja" << endl;
+    cout << "3. Brisanje čvora iz dvostruko vezane liste" << endl;
+    cout << "4. Dodavanje novog čvora u binarno stablo, ispis čvorova stabla" << endl;
+    cout << "9. Izlaz iz programa" << endl;
+    cout <<"----------------------------------------------------------------------" << endl;
+    cin >> jos;
+    switch(jos){
+        case 1:
+            
+            break;
+        case 2:
+            
+            break;
+        case 3:
+            
+            break;
+        case 4:
+            
+            break;
+    }
+}while(jos != 9);
+ 
+ 
+    return 0;
+}
+ 
