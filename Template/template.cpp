29130afa9333d47890cf750c783d3f01978cc557
Rysc0 <boris.bastek@gmail.com>
2020-11-10 20:57:21 +0100

    Made template for my tasks

---
+++ Template/template.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "biblioteka_vrijeme.cc"
+
+using namespace std;
+  
+
+ 
+int main(){
+
+
+
+int jos;
+do{
+    cout <<"----------------------------------------------------------------------" << endl;
+    cout << "1. " << endl;
+    cout << "2. " << endl;
+    cout << "3. " << endl;
+    cout << "4. " << endl;
+    cout << "9. Izlaz iz programa" << endl;
+    cout <<"----------------------------------------------------------------------" << endl;
+    cin >> jos;
+    switch(jos){
+        case 1:
+            // function
+            break;
+        case 2:
+            // function
+            break;
+        case 3:
+            // function
+            break;
+        case 4:
+            // function
+            break;
+    }
+}while(jos != 9);
+ 
+ 
+    return 0;
+}
